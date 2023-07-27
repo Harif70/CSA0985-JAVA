936d4c44e73b1c0ba786de3306ed6d810263c685
Harif70 <114127754+Harif70@users.noreply.github.com>
2023-07-27 08:11:10 +0530

    Add files via upload

---
+++ ifelse1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+public class ifelse1{
+public static void main(){
+int num;
+Scanner a=new Scanner(System.in);
+System.out.println("Enter a number);
+num=a.nextInt();
+if(num/2==0){
+system.out.println("Its a even value");
+}else{
+System.out.println("Its a odd number");
+}
+}
+} 
